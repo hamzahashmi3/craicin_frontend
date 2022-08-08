f313fc0d14da7bdf105341c78b154de4decd95b5
Adnan Tahir <adnantahir1998@gmail.com>
2022-08-08 18:35:50 +0500

    Revert "More Post Api Integrations with UI"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import {useState} from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Navbar from './Components/Navbar'
 import Home from './Screens/Home'
-import Tour from './Screens/ToursDetails'
+import Tour from './Screens/Tours'
 import About from './Screens/About'
 import Contact from './Screens/Contact'
 import Checkout from './Screens/Checkout'
@@ -22,6 +23,14 @@ import Faq from './Pages/Faq'
 
 function App() {
 
+  const [inputText, setInputText] = useState("")
+  const [selectAdults, setSelectAdults] = useState("")
+  const [selectChildren, setSelectChildren] = useState("")
+  const [selectSenior, setSelectSenior] = useState("")
+  const [selectStudents, setSelectStudents] = useState("")
+  const [date, setDate] = useState(new Date('Pick Your Tour Date'))
+  const [tourBooking, setTourBooking] = useState([])
+
   return (
     <div>
       <Navbar />
@@ -31,10 +40,26 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/payment" element={<Payment />} />
-        <Route path="/tourdetails" element={<Tour />} />
+        <Route path="/tour" 
+          element={
+          <Tour 
+            inputText={inputText}
+            setInput={setInputText}
+            selectAdults={selectAdults}
+            setSelectAdults={setSelectAdults}
+            selectChildren={selectChildren}
+            setSelectChildren={setSelectChildren}
+            selectSenior={selectSenior}
+            setSelectSenior={setSelectSenior}
+            selectStudents={selectStudents}
+            setSelectStudents={setSelectStudents}
+            date={date}
+            setDate={setDate}
+            tourBooking={tourBooking}
+            setTourBooking={setTourBooking} />}  
+          />
         <Route path="/*" element={<Page404 />} />
 
-        {/* Footer Pages */}
         <Route path="/conferenceservices" element={<ConferenceServices />} />
         <Route path="/agentsaffiliates" element={<AgentsAffiliates />} />
         <Route path="/privacycookies" element={<PrivacyCookies />} />
